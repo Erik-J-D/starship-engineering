ca1abe3a84558284bf097adbfcac7a5315c8ffa9
David Rusu <davidrusu.me@gmail.com>
2021-05-15 14:58:06 -0400

    Typescript is accepting p5's global variables

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as p5 from "p5";
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
